10f7c6f5aae570d14d0adbd36cb89526889aaafa
Carlos Mattoso <calmattoso@hotmail.com>
2014-03-26 22:47:45 -0300

    Test logging with simple header.

--- T1/src/main.cpp
+++ T1/src/main.cpp
@@ -9,16 +9,16 @@
 using namespace std;
 
 int main(){
-  string strMain  = ReadFile("\\maps\\mapa_zelda1.txt"),
-       strDungeon1 = ReadFile("mapa_dungeon1.txt"),
-       strDungeon2 = ReadFile("\\maps\\mapa_dungeon2.txt"),
-       strDungeon3 = ReadFile("\\maps\\mapa_dungeon3.txt");
+  string strMain     = ReadFile("../maps/mapa_zelda1.txt"  ),
+         strDungeon1 = ReadFile("../maps/mapa_dungeon1.txt"),
+         strDungeon2 = ReadFile("../maps/mapa_dungeon2.txt"),
+         strDungeon3 = ReadFile("../maps/mapa_dungeon3.txt") ;
    
   Map dungeon1( strDungeon1 , 28 );
   dungeon1.Display(true);
   vector< Coord > solution = dungeon1.Solve(make_pair(0,0), make_pair(1,1));
 
-  LogSolution("dg1_log.txt", solution);
+  LogSolution("../logs/dg1_log.txt", "Dungeon 1", solution);
 
 
   return 0;
