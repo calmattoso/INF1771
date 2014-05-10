9586d912cf5cc53bd6ead97aff0eebdd53685eb2
Kaplan, L <contato.leonardokaplan@gmail.com>
2014-05-09 23:08:58 -0300

    sense = nil

--- T2/prolog.lua
+++ T2/prolog.lua
@@ -3,27 +3,54 @@
 --static area (will not changed based on the map values)
 
 --link actions, sorted by priority
+--[[
+se tem rupee, pega.
+se tem espada, pega
+se tem coracao e energia<50, pega.	
+se tem monstro adjacente e energia maior que 10, ataca.
+se em frente estiver seguro, anda.
+vira.
+--]]
 print("
-walk() :-
-turn_right() :-
+best_action(X,Y,FX,FY,Energy) :- pickup_rupee(X,Y).
+best_action(X,Y,FX,FY,Energy) :- pickup_sword(X,Y).
+best_action(X,Y,FX,FY,Energy) :- pickup_heart(X,Y), Energy<50.
+best_action(X,Y,FX,FY,Energy) :- atack(FX,FY), Energy>10.
+best_action(X,Y,FX,FY,Energy) :- walk(FX,FY).
+best_action(X,Y,Energy) :- turn_right().
+
+%best_action(X,Y,Energy) :- facing(X+1,Y), atack(X+1,Y), Energy>10.
+%best_action(X,Y,Energy) :- facing(X-1,Y), atack(X-1,Y), Energy>10.
+%best_action(X,Y,Energy) :- facing(X,Y+1), atack(X,Y+1), Energy>10.
+%best_action(X,Y,Energy) :- facing(X,Y-1), atack(X,Y-1), Energy>10.
+
+%best_action(X,Y,Energy) :- facing(X+1,Y), safe(X+1,Y).
+%best_action(X,Y,Energy) :- facing(X-1,Y), safe(X-1,Y).
+%best_action(X,Y,Energy) :- facing(X,Y+1), safe(X,Y+1).
+%best_action(X,Y,Energy) :- facing(X,Y-1), safe(X,Y-1).
+
+
+walk() :- muda x = fx ,y = fy
+turn_right() :- cicla entre os 4 estados
 turn_left() :-
-atack() :-
-pickup_heart() :-
-pickup_rupee() :-
-pickup_sword() :-
+atack(X,Y) :- 
+pickup_heart(X,Y) :- heart(X,Y).
+pickup_rupee(X,Y) :- rupee(X,Y).
+pickup_sword(X,Y) :- sword(X,Y).
 ")
 --map based area
 item = {
 [1] = "rupee",
-[2] = "realsword",
-[3] = "sword",
+[2] = "sword",
 [4] = "heart",
+[4] = "sword",--"realsword",
 [5] = "hole",
 [6] = "monster",
 [7] = "portal"
 }
 
 --read map using readmap from T1
+
 m = #map
 n = #map[1]
 mapsize = m*n
