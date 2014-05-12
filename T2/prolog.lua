910aa72df43180bde19931e9b82e55e3e64891d6
Kaplan, L <contato.leonardokaplan@gmail.com>
2014-05-12 20:28:13 -0300

    yet another fix

--- T2/prolog.lua
+++ T2/prolog.lua
@@ -22,25 +22,31 @@ io.write(header)
 
 for x=1,#map do
 	for y=1,#map[1] do
- 		if (map[x][y] == "1") then io.write(string.format("pos(%s,%s).",x,y)) end
+ 		if map[x][y] == 1 then 
+ 			io.write(string.format("pos(%s,%s).\n",x,y)) 
+ 		end
 	end
 end
 for line in io.lines("items.log") do 
-	_,_,x,y,item_tmp = string.find(line, "(%d+)%s(%d+)%s(%a)")
-	if item_temp =="B" or item_temp =="E" or item_temp =="V" then 
-			if x < 42 then
-				if y < 42 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x+1,y+1)) end
-				if x>0 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x+1,y-1)) end
-			end
-			if x>0 then
-				if y < 42 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x-1,y+1)) end
-				if x>0 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x-1,y-1)) end
-			end
-	else 
-		io.write(string.format("at(%s,pos(%s,%s)).\n",sensor_id[item_tmp],x,y))
-	end
-	if item_temp =="B" or item_temp =="E" or item_temp =="V" or item_temp =="M" then 
-		io.write(string.format("at(%s,pos(%s,%s)).\n",item_id[item_tmp],x,y)) 
+	_,_,y,x,item_tmp = string.find(line, "(%d+)%s(%d+)%s(%a)")
+	y = y + 1
+	x = x + 1
+	if map[x][y] == 1 then
+		if item_temp =="B" or item_temp =="E" or item_temp =="V" then 
+				if x < 42 then
+					if y < 42 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x+1,y+1)) end
+					if x>0 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x+1,y-1)) end
+				end
+				if x>0 then
+					if y < 42 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x-1,y+1)) end
+					if x>0 then io.write(string.format("%s(%s,%s).\n",sensor_id[item_tmp],x-1,y-1)) end
+				end
+		else 
+			io.write(string.format("at(%s,pos(%s,%s)).\n",sensor_id[item_tmp],x,y))
+		end
+		if item_tmp =="B" or item_tmp =="E" or item_tmp =="V" or item_tmp =="M" then 
+			io.write(string.format("at(%s,pos(%s,%s)).\n",item_id[item_tmp],x,y)) 
+		end
 	end
 end
 
