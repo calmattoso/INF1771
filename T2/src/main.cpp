24e6d7fe8119df408a7f7d131c7dc57a7173a6f1
Carlos Mattoso <calmattoso@hotmail.com>
2014-05-12 18:13:11 -0300

    Generate log of Prolog code executed; generate log for visualization code.

--- T2/src/main.cpp
+++ T2/src/main.cpp
@@ -13,6 +13,52 @@
 
 using namespace std;
 
+void adjust_string(string& str, string hint)
+{
+
+  /* Move */
+    if(str.find("move(") != string::npos)
+      str = "move_to(void," + str.substr(5, string::npos);
+
+  /* Attack monster */
+    else if(str.find("attack_monster(") != string::npos)
+      str = "attack_monster(void," + str.substr(15, string::npos);
+
+  /* We receive simply a position, adjust it to safe */
+    else if(str.find("pos(") != string::npos)
+        str = hint + "(void," + str.substr(4, string::npos);
+
+  /* Game Won */
+    else if( str == "won" )
+      str = "won(void,void,void)";
+
+  /* Game Lost */
+    else if( str == "like_omg_dead")
+      str = "dead(void,void,void)";
+}
+
+void check_danger(string dangerCode , ofstream& log)
+{
+  PlTermv term(2);
+  term[0] = PlCompound( dangerCode.c_str() );
+  PlQuery query("at", term);
+
+  string hint = "actual_danger";
+  if( dangerCode.find("potencial") != string::npos )
+  {
+    hint = "potential_danger";
+  }
+
+  while( query.next_solution() )
+  {
+    string pos( ( char* )(term[1]) ); 
+
+    adjust_string( pos , hint );
+
+    log << pos << "\n";
+  }
+}
+
 int main( )
 {
   char * argv[] = {
@@ -25,9 +71,16 @@ int main( )
   bool not_over = true;
   ofstream log_prolog("../logs/prolog.log"),
            log_vis("../logs/vis.log");
+  string dangerCodes[6] = {
+    "potencial_monster", "potencial_hole", "potencial_vortex",
+    "actual_monster", "actual_hole", "actual_vortex" 
+  };
 
-  if( !log_prolog.is_open() && !log_vis.is_open() )
-  {
+  if( !log_vis.is_open() 
+#ifdef _LOG_PROLOG
+      && !log_prolog.is_open() 
+#endif
+  ){
     cout << "Could not open log files! Terminating...\n";
     exit(1);
   }
@@ -38,6 +91,7 @@ int main( )
 
   while( not_over )
   {
+
     /* Link's gotta sense something */
       PlTermv * senseTerm = new PlTermv(1);
       PlQuery * senseQuery = new PlQuery("sense", *senseTerm);
@@ -47,11 +101,10 @@ int main( )
         /* display what sense yielded, and store that */
           string senseOutcome( (( char* ) (*senseTerm)[0]) );
 
-#ifdef _LOG_PROLOG
+      #ifdef _LOG_PROLOG
           log_prolog << "sense(X)\n" << "  X = " << senseOutcome << "\n";
           log_prolog << senseOutcome << "\n";
-#endif
-          log_vis << senseOutcome << "\n";
+      #endif
 
         /* destroy the query and term */
           delete senseQuery;
@@ -70,6 +123,26 @@ int main( )
         delete senseTerm;
       }
 
+    /* Show what's safe, potencial danger and actual danger */
+      PlTermv * safeTerm  = new PlTermv(1);
+      PlQuery * safeQuery = new PlQuery("safe", *safeTerm);
+
+        while( safeQuery->next_solution() )
+        {
+          string safePos( (( char* ) (*safeTerm)[0]) );
+
+          adjust_string( safePos , "safe" );
+
+          log_vis << safePos << "\n";
+        }
+
+      delete safeTerm;
+      delete safeQuery;
+
+      for( int i = 0; i < 6; i++ )
+        check_danger( dangerCodes[i] , log_vis);      
+
+
     /* Now, Link's gotta do some action */
       PlTermv * actionTerm  = new PlTermv(1);
       PlQuery * actionQuery = new PlQuery("best_action", *actionTerm);
@@ -78,19 +151,22 @@ int main( )
       {
         /* display which action best_action yielded, and store that */
           string actionOutcome( (( char* ) (*actionTerm)[0]) );
+          string visString = actionOutcome;
 
-#ifdef _LOG_PROLOG
+          adjust_string(visString,"");
+
+        #ifdef _LOG_PROLOG
           log_prolog << "best_action(X)\n" << "  X = " << actionOutcome << "\n";
-#endif
-          log_vis << actionOutcome << "\n";
+        #endif
+          log_vis << visString << "\n";
 
           if( actionOutcome == "won" || 
               actionOutcome == "like_omg_dead" )
             break;
 
-#ifdef _LOG_PROLOG
+        #ifdef _LOG_PROLOG
           log_prolog << actionOutcome << "\n";
-#endif
+        #endif
 
 
         /* destroy the query and term */
